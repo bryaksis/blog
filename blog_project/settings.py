8430d1071e786b91a6d0dd2cf570b581cdcf367e
ahmet.adiguzel <ahmet.adiguzel@tubitak.gov.tr>
2018-11-01 16:58:10 +0300

    *

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
